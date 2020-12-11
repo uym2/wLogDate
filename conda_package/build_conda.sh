1bbf84872e13fa8aaee281f72457081a09aa8df8
Mai To Uyen <uym2@Mais-MBP-3.attlocal.net>
2020-12-11 11:20:04 -0800

    update conda package to 1.0.2

--- conda_package/build_conda.sh
+++ conda_package/build_conda.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
 v=$1
-sed -i -e "s/version:.*$/version: $v/g" wlogdate/meta.yaml
-
 conda-build wlogdate/
 conda convert --platform all //anaconda3/conda-bld/osx-64/wlogdate-$v*.tar.bz2 -o //anaconda3/conda-bld/
 anaconda upload --force //anaconda3/conda-bld/*/wlogdate-$v*.tar.bz2
